d53ebdb99771f6247e168629a8be5a537218fbb6
etherneel <sswami610@gmail.com>
2024-03-23 17:14:01 +0530

    Code changed

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
  * @description: It Contain server setup functions.
  * @author: Manthan Vaghasiya
  */
-
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
